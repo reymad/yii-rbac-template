32ff73b0e5b64c552df8471b2cce402a604b98ee
jrey <jrey@proximity.es>
2017-02-13 12:11:19 +0100

    Mas cositas

--- app/config/params.php
+++ app/config/params.php
@@ -2,4 +2,6 @@
 
 return [
     'adminEmail' => 'admin@example.com',
+    'idioma_default' => 'es-ES',
+    'idiomas'        => ['es-ES','en-US'],
 ];
--- app/controllers/MyController.php
+++ app/controllers/MyController.php
@@ -9,6 +9,7 @@
 namespace app\controllers;
 
 use Yii;
+use yii\helpers\ArrayHelper;
 use yii\web\Controller;
 
 
@@ -19,9 +20,57 @@ class MyController extends Controller
     {
         parent::init();
         #add your logic: read the cookie and then set the language
-        // de momento español aqui irá la lógica de idiomas
-        \Yii::$app->language = 'es-ES';
+        $this->setLanguage();
 
     }
+
+    private function setLanguage(){
+
+        if( ($lang = Yii::$app->request->get('lang')) && (ArrayHelper::isIn(Yii::$app->request->get('lang'), Yii::$app->params['idiomas']) ) )
+        {
+            $this->setCookie([
+                'name' => 'lang',
+                'value' => $lang,
+                'expire' => time() + 86400 * 365,
+            ]);
+            $this->setLang($lang);
+        }
+        else if($lang = $this->getCookie('lang'))
+        {
+            $this->setLang($lang);
+        }
+        else
+        {
+            $this->setLang(Yii::$app->params['idioma_default']);
+        }
+
+    }
+
+    /*
+     * Cookie management to all children classes
+     * */
+    public function getCookie($cookieName){
+        $cookies = Yii::$app->request->cookies;
+        if ($cookies->has($cookieName))
+            return $cookies->getValue($cookieName);
+        return false;
+    }
+
+    public function setCookie($params=[]){
+        $cookies = Yii::$app->response->cookies;
+        $cookies->add(new \yii\web\Cookie($params));
+    }
+
+    /*
+     * Lang setters
+     * */
+    public function getLang(){
+        return Yii::$app->language;
+    }
+
+    public function setLang($lang){
+        Yii::$app->language = $lang;
+    }
+
 
 }
