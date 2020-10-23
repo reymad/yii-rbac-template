8378e615c1deb2d68cec98ce35144b884e7212ed
reymad <jesusrey85@gmail.com>
2020-10-23 08:40:39 +0200

    Update db.php

--- app/config/db.php
+++ app/config/db.php
@@ -4,6 +4,6 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii-basic',
     'username' => 'root',
-    'password' => 'put0am0',
+    'password' => 'pu*0*m0',
     'charset' => 'utf8',
 ];
